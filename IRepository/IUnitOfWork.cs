153568a270267bfad3847f0ac4db379b612ac034
ems <ems82@ya.ru>
2021-06-12 14:22:13 +0300

    Repository

---
+++ IRepository/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+using EmployeeDirectory.Data;
+
+namespace EmployeeDirectory.IRepository
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IGenericRepository<Department> Departments { get; }
+        IGenericRepository<Position> Positions { get; }
+        IGenericRepository<Employee> Employees { get; }
+        Task Save();
+    }
+}
---
+++ Repository/UnitOfWork.cs
@@ -0,0 +1,35 @@
+﻿using EmployeeDirectory.IRepository;
+using System;
+using System.Threading.Tasks;
+using EmployeeDirectory.Data;
+
+namespace EmployeeDirectory.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly DatabaseContext _context;
+
+        private IGenericRepository<Department> _department;
+        private IGenericRepository<Position> _position;
+        private IGenericRepository<Employee> _employee;
+
+        public UnitOfWork(DatabaseContext context)
+        {
+            _context = context;
+        }
+        public IGenericRepository<Department> Departments => _department ??= new GenericRepository<Department>(_context);
+        public IGenericRepository<Position> Positions => _position ??= new GenericRepository<Position>(_context);
+        public IGenericRepository<Employee> Employees => _employee ??= new GenericRepository<Employee>(_context);
+
+        public void Dispose()
+        {
+            _context.Dispose();
+            GC.SuppressFinalize(this);
+        }
+
+        public async Task Save()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
